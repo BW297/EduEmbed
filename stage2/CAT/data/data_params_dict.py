829e53d6d373430f4d6fc0b4719684110f4bbc1a
zhouzh114 <zhouzh114>
2025-05-21 11:56:02 +0800

    correct

--- stage2/CAT/data/data_params_dict.py
+++ stage2/CAT/data/data_params_dict.py
@@ -1,33 +1,21 @@
 data_params = {
-    'SLP-CHI':{
+    'SLP-Chi':{
         "stu_num": 623,
         "prob_num": 510,
         "know_num": 17,
-        "batch_size": 16
+        "batch_size": 32
     },
-    'SLP-MAT':{
+    'SLP-Math':{
         "stu_num": 1080,
         "prob_num": 609,
         "know_num": 32,
-        "batch_size": 16
+        "batch_size": 32
     },
-    'NIPS20':{
-        "stu_num": 300,
-        "prob_num": 900,
-        "know_num": 85,
-        "batch_size": 16
-    },
-    'EDM-CUP-23sp1':{
-        "stu_num": 300,
-        "prob_num": 1167,
-        "know_num": 250,
-        "batch_size": 16
-    },
-    'XES3G5M':{
-        "stu_num": 300,
-        "prob_num": 5544,
-        "know_num": 680,
-        "batch_size": 16
+    'SLP-Math-induct':{
+        "stu_num": 1080,
+        "prob_num": 609,
+        "know_num": 32,
+        "batch_size": 32
     }
     
 }
